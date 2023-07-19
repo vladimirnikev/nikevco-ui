9fbab8ebb6aeb1c39a8fe04bf8c5c8aeed66dae7
vladimirnikev <vladimirnikev@gmail.com>
2023-07-19 23:03:16 +0300

    create modal module

---
+++ projects/nikevco-ui/src/lib/modules/modal/modal.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class ModalModule { }
+
+// TODO
+
+// Modal dialog title
+// Modal dialog content
+// Modal dialog actions
+// Modal close button
+// Modal service
